2948c0cf75a938271a3a651290b78a3d274fee8b
유하람 <w2036@e-mirim.hs.kr>
2021-04-15 20:44:19 +0900

    Excercise 4_7예제 finish

--- app/src/main/java/kr/hs/emirim/w2036/exercise4_6/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2036/exercise4_6/MainActivity.java
@@ -5,10 +5,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    CheckBox checkEnabled, checkClickable, checkLotation;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.ex_7);
+        checkEnabled = findViewById(R.id.check_enabled);
+        checkClickable = findViewById(R.id.check_clickable);
+        checkRotation = findViewById(R.id.check_rotation);
+
+        btn = findViewById(R.id.btn);
+
+        checkEnabled.setOnCheckedChangeListener(checkListener);
+        checkClickable.setOnCheckedChangeListener(checkListener);
+        checkRotation.setOnCheckedChangeListener(checkListener);
     }
+    CompoundButton.OnCheckedChangeListener checkListener = new CompoundButton.OnCheckedChangeListener(){
+        @override
+        public void onCheckedChanged(CompoundButton v, boolean isChecked){
+            switch(v,getId()){
+                case R.id.check_enabled:
+                    if(isChecked){
+                        btn.setEnabled(true);
+                    }else{
+                        btn.setEnabled(false);
+                    }
+                    break;
+
+                case R.id.check_clickable:
+                    if(isChecked){
+                        btn.setClickable(true);
+                    }else{
+                        btn.setClickable(false);
+                    }
+                    break;
+                case R.id.check_rotation:
+                    if(isChecked){
+                        btn.setRotation(45);
+                    }else{
+                        btn.setRotation(0);
+                    }
+                    break;
+
+            } //end of switch
+        }
+    };
 }
